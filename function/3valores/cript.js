510267f6f273c86b693c0be05e1f12dd4996a1a0
Carlos Daniel Pereira <141742674+CarlosDanielPereira@users.noreply.github.com>
2024-10-17 15:37:28 -0300

    Add files via upload

---
+++ function/3valores/cript.js
@@ -0,0 +1,11 @@
+
+function valores(){
+    let n1 = Number(prompt("Digite um valor"))
+    let n2 = Number(prompt("Digite outro valor"))
+    let n3 = Number(prompt ("Digite outro valor"))
+    let media = (n1 + n2 + n3) / 3
+    console.log(media)
+    return media
+}
+
+valores()
